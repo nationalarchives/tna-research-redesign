1b60372f527bacb85ee4a5fcbd233fd4f67f4dd0
Punal Chotrani <punalsc@gmail.com>
2017-05-16 18:41:03 +0100

    Updating landing page template.

--- page-research-repository-landing.php
+++ page-research-repository-landing.php
@@ -10,15 +10,66 @@ get_header();
         <div class="container">
             <div class="row">
                 <main id="main" class="col-xs-12 col-sm-8 col-md-8" role="main">
-					<?php
-					while ( have_posts() ) : the_post();
-						get_template_part( 'content' );
-					endwhile;
-					?>
-                    <div class=" col-md-12">
-                        <p>
-                            The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                        </p>
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                            <div class="entry-header img-container"
+                                 style="background-image: url('http://placehold.it/793x300')">
+                                <h1><?php the_title(); ?></h1>
+                            </div>
+                            <div class="entry-content clearfix">
+								<?php the_content(); ?>
+                            </div>
+                        </article>
+					<?php endwhile; endif; ?>
+                    <div class="col-xs-12 col-md-12 secondary-content">
+                        <div class="content-box">
+                            <a href="#"><h3>This is a title</h3></a>
+                            <span class="entry-meta">
+                                <strong>Author:</strong>
+                            </span>
+                            <span class="entry-meta">
+                                John Doe
+                            </span>
+                            <p>
+                                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those
+                                interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero
+                                are also reproduced in their exact original form, accompanied by English versions from
+                                the 1914 translation by H. Rackham.
+                            </p>
+                            <hr>
+                        </div>
+                        <div class="content-box">
+                            <a href="#"><h3>This is a title</h3></a>
+                            <span class="entry-meta">
+                                <strong>Author:</strong>
+                            </span>
+                            <span class="entry-meta">
+                                John Doe
+                            </span>
+                            <p>
+                                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those
+                                interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero
+                                are also reproduced in their exact original form, accompanied by English versions from
+                                the 1914 translation by H. Rackham.
+                            </p>
+                            <hr>
+                        </div>
+                        <div class="content-box">
+                            <a href="#"><h3>This is a title</h3></a>
+                            <span class="entry-meta">
+                                <strong>Author:</strong>
+                            </span>
+                            <span class="entry-meta">
+                                John Doe
+                            </span>
+                            <p>
+                                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those
+                                interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero
+                                are also reproduced in their exact original form, accompanied by English versions from
+                                the 1914 translation by H. Rackham.
+                            </p>
+                            <hr>
+                        </div>
                     </div>
                 </main>
 				<?php
