49bab9b282e3f81e2dac32704996e5ddae1ec813
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2017-05-24 15:29:32 +0100

    Removed the space in front of the semicollon

--- page-research-repository-details.php
+++ page-research-repository-details.php
@@ -26,7 +26,7 @@ get_header(); ?>
                                 }
                                 echo ' ' . $lead_author;
                                 if ( ! empty ( $other_authors ) ) {
-	                                echo ' ; ' . $other_authors;
+	                                echo '; ' . $other_authors;
                                 }
                                 ?>
                                 </span>
