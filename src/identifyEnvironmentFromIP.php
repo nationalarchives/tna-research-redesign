d5a785300f2f98c4d286cfed2f847f05848ff19d
karlkern <karl.kern@nationalarchives.gov.uk>
2026-03-24 10:59:00 +0000

    change header

--- src/identifyEnvironmentFromIP.php
+++ src/identifyEnvironmentFromIP.php
@@ -11,7 +11,7 @@
 function identifyEnvironmentFromIP($server_ip = null, $client_ip = null)
 {
     $headers = apache_request_headers();
-    if ( isset($_SERVER['HTTP_X_NGINX_PROXY']) && isset($headers['X_HOST_TYPE']) && $headers['X_HOST_TYPE'] == 'public' ) {
+    if ( isset($_SERVER['HTTP_X_NGINX_PROXY']) && isset($headers['X-Host-Type']) && $headers['X-Host-Type'] == 'public' ) {
         return 'aws_public';
     }
 
